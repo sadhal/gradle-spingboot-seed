3e23f13a6a0df8d20ef66b64cfbfd350ff6fca2d
Sadmir Halilovic <halilovic.sadmir@gmail.com>
2016-12-28 00:40:10 +0100

    added rest resource and server config in yml file

---
+++ src/main/java/se/sadhal/RestResource.java
@@ -0,0 +1,21 @@
+package se.sadhal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RestResource {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestResource.class);
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        LOG.info("/hello called");
+
+        return "Hello, world!";
+    }
+}
---
+++ src/test/java/se/sadhal/RestResourceTest.java
@@ -0,0 +1,15 @@
+package se.sadhal;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sadmir on 2016-12-28.
+ */
+public class RestResourceTest {
+    @org.junit.Test
+    public void hello() throws Exception {
+        RestResource rr = new RestResource();
+        assertEquals("Hello, world!", rr.hello());
+    }
+
+}
