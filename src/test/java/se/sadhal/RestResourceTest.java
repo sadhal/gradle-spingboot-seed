b7d5765744f8602eb16d8c075d3f0464f828f9fc
Sadmir Halilovic <sadhal@users.noreply.github.com>
2017-01-28 23:49:35 +0100

    updated test

--- src/test/java/se/sadhal/RestResourceTest.java
+++ src/test/java/se/sadhal/RestResourceTest.java
@@ -9,7 +9,7 @@ public class RestResourceTest {
     @org.junit.Test
     public void hello() throws Exception {
         RestResource rr = new RestResource();
-        assertEquals("Hello, openshift developer preview (NextGen)!", rr.hello());
+        assertEquals("Hello, world!", rr.hello());
     }
 
 }
