82c8e41de6552b8e9f038c19208f157129bea76c
Sadmir Halilovic <sadhal@users.noreply.github.com>
2017-02-01 22:36:18 +0100

    hello world response

--- src/main/java/se/sadhal/RestResource.java
+++ src/main/java/se/sadhal/RestResource.java
@@ -27,7 +27,7 @@ public class RestResource {
     public String hello() {
         LOG.info("/hello called");
 
-        return "Hello, openshift!";
+        return "Hello, world!";
     }
 
     @RequestMapping(value = "/healthz", method = RequestMethod.GET)
